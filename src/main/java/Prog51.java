4e83b8ec34917974e3555e4b720dd5d9cdd57fed
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-09 18:03:46 +0900

    Update Prog51.java
    replacement of tabs with 4 spaces

--- src/main/java/Prog51.java
+++ src/main/java/Prog51.java
@@ -1,8 +1,8 @@
 public class Prog51 {
 
-	public static void main(String[] args) {
-		String name = args[0];
-		String telNumber = args[1];
-	}
+    public static void main(String[] args) {
+        String name = args[0];
+        String telNumber = args[1];
+    }
 }
 
